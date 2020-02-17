7e7fd81b01d3aa5c4fb62c2354841bfbe95f99d1
Shtekata <gesheval@gmail.com>
2020-02-17 21:26:54 +0200

    17

---
+++ 17. JSON PROCESSING/JSONDemo/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace JSONDemo
+{
+    public class ProductDTO
+    {
+        [JsonProperty("Title")]
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public int? SellerId { get; set; }
+
+        public int? BuyerId { get; set; }
+    }
+}
---
+++ 17. JSON PROCESSING/JSONDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace JSONDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var inputJson = File.ReadAllText("./../../../products.json");
+
+            //var obj = JObject.Parse(inputJson)["products"]
+            //    .Select(x => $"{x["Title"]}: {x["Price"]}")
+            //    .ToList();
+
+            //Console.WriteLine(String.Join(Environment.NewLine, obj));
+
+            //var obje= JObject.Parse(inputJson);
+
+            //foreach (JToken token in obje["products"])
+            //{
+            //    Console.WriteLine(token["Title"]);
+            //    Console.WriteLine(token["Price"]);
+            //    Console.WriteLine(token["SellerId"]);
+            //    Console.WriteLine(token["BuyerId"]);
+            //    Console.WriteLine();
+            //}
+
+            //var products = JsonConvert.DeserializeObject<ProductDTO[]>(inputJson);
+
+            //var resolver = new DefaultContractResolver()
+            //{
+            //    NamingStrategy = new SnakeCaseNamingStrategy()
+            //};
+
+            //var againJson = JsonConvert.SerializeObject(products, new JsonSerializerSettings() { ContractResolver = resolver, Formatting = Formatting.Indented });
+
+            //Console.WriteLine(againJson);
+
+            var inputXml = File.ReadAllText("./../../../products.xml");
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(inputXml);
+
+            var json = JsonConvert.SerializeObject(xmlDoc, Newtonsoft.Json.Formatting.Indented);
+
+            Console.WriteLine(json);
+        }
+    }
+}

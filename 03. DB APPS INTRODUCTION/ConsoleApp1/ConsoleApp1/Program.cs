3f766336ef7f7744c3373cdee29756177d69be61
Shtekata <gesheval@gmail.com>
2020-02-17 21:13:55 +0200

    03

---
+++ 03. DB APPS INTRODUCTION/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace AdoNetTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    class Program
+    {
+        static void Main()
+        {
+            var list = new List<int>();
+
+            list ??= new List<int>();
+
+            static int LocalFunction(int x, int y) => x + y;
+            LocalFunction(5, 5);
+
+            using var connection = new SqlConnection(@"Server=.\SQLEXPRESS01;Database=SoftUni;Integrated Security=True;");
+
+            connection.Open();
+
+            //using (connection)
+            //{
+                var firstName = "Kevin";
+                var department = "7";
+                var command = new SqlCommand("select * from Employees where FirstName = @name and DepartmentId   = @department", connection);
+                command.Parameters.AddWithValue("@name", firstName);
+                command.Parameters.AddWithValue("@department", department);
+                //var result = command.ExecuteScalar();
+                //Console.WriteLine(result);
+
+                var reader = command.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        //var firstName = reader["FirstName"];
+                        //var lastName = reader["LastName"];
+                        //var salary = reader["Salary"];
+                        //Console.WriteLine($"{firstName} {lastName} {salary:f2}");
+
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader[i]} ");
+                        }
+                        Console.WriteLine();
+                    }
+                //}
+
+                //command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ 03. DB APPS INTRODUCTION/ConsoleApp1/ConsoleApp1/Town.cs
@@ -0,0 +1,9 @@
+﻿namespace AdoNetTest
+{
+    public class Town
+    {
+        public int TownId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
